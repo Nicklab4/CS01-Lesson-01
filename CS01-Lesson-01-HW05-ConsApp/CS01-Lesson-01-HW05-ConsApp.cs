10ec50a363a94ec05386b99ff91c2b701ed0e1d5
Nicklab4 <108210847+Nicklab4@users.noreply.github.com>
2024-09-03 00:14:16 +0500

    Commit 5го ДЗ

--- CS01-Lesson-01-HW05-ConsApp/CS01-Lesson-01-HW05-ConsApp.cs
+++ CS01-Lesson-01-HW05-ConsApp/CS01-Lesson-01-HW05-ConsApp.cs
@@ -19,11 +19,25 @@ namespace CS01_Lesson_01_HW05_ConsApp
             Console.WriteLine("Назовите ваше Имя:");
             string name = Console.ReadLine();
             Console.WriteLine("Назовите вашу Фамилию:");
-            int age = int.Parse(Console.ReadLine());
+            string lastname  = Console.ReadLine();
             Console.WriteLine("Назовите ваш город:");
             string town = Console.ReadLine();
+            string finalS = name + " "+ lastname + " живёт в городе " + town;
 
+            Cn.Print(finalS, (Console.BufferWidth - finalS.Length) / 2,
+                Console.CursorTop + 1);
 
+            Console.ReadLine();
         }
+    }
+
+    static class Cn
+    {
+        public static void Print(string ms, int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(ms);
+        }
+
     }
 }
